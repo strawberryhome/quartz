4f6fd579c9bf915b01393162ada0742bddb2e1f1
amber <a06000208@protonmail.com>
2026-02-25 15:51:20 -0600

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
     pageTitle: "Strawberry Blog",
     pageTitleSuffix: "",
     enableSPA: true,
-    enablePopovers: true,
+    enablePopovers: false,
     analytics: null,
     locale: "en-US",
     baseUrl: "blog.strawberryhome.org",
