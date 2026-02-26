4febf27b534c49b463a3ac9e624f763c2f451979
amber <a06000208@protonmail.com>
2026-02-25 21:52:45 -0600

    Create previews script

---
+++ scripts/previews.js
@@ -0,0 +1,37 @@
+import { globby, convertPathToPattern } from "globby";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { JSDOM } from "jsdom";
+import { readFile, writeFile } from "node:fs/promises";
+
+const previewsDirectory = join(dirname(fileURLToPath(import.meta.url)), "..", "previews");
+const search = convertPathToPattern(join(previewsDirectory, "**/*.html"))
+const files = await globby(search);
+console.log(`Rewriting ${files.length} files`)
+
+try {
+    for (const filePath of files) {
+        const data = await readFile(filePath, { encoding: "utf8" });
+        if (!data) continue;
+        /** @type {JSDOM} */
+        const dom = new JSDOM(data);
+        dom.window.document.body.innerHTML = "<p>preview page intended for bots</p>";
+        const queries = [
+            "script",
+            "style",
+            `link[rel="stylesheet"]`,
+            `link[rel="preconnect"]`,
+            `link[title="RSS Feed"]`,
+        ]
+        for (const query of queries) {
+            const elements = dom.window.document.querySelectorAll(query);
+            for (const element of elements) {
+                element.remove();
+            }
+        }
+        const content = dom.serialize();
+        await writeFile(filePath, content, { encoding: "utf8" });
+    };
+} catch (error) {
+    console.error(error);
+}
